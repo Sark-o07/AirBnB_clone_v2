36ed6e54d7e9ff896397cc886e46ad479305ef22
Sark-o07 <kosinick01@gmail.com>
2024-01-04 03:56:43 -0800

    updating the version2

--- models/place.py
+++ models/place.py
@@ -1,83 +1,109 @@
 #!/usr/bin/python3
-"""This is the place class"""
-from sqlalchemy.ext.declarative import declarative_base
+""" Place Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Table, String, Integer, Float, ForeignKey
+#from models.amenity import Amenity
+from sqlalchemy import (
+    Column,
+    String,
+    ForeignKey,
+    Integer,
+    Float,
+    Table
+    )
 from sqlalchemy.orm import relationship
 from os import getenv
-import models
 
+storage_type = getenv("HBNB_TYPE_STORAGE")
 
-place_amenity = Table("place_amenity", Base.metadata,
-                      Column("place_id", String(60),
-                             ForeignKey("places.id"),
-                             primary_key=True,
-                             nullable=False),
-                      Column("amenity_id", String(60),
-                             ForeignKey("amenities.id"),
-                             primary_key=True,
-                             nullable=False))
+place_amenity = Table(
+        'place_amenity',
+        Base.metadata,
+        Column(
+            'place_id',
+            String(60),
+            ForeignKey('places.id'),
+            primary_key=True,
+            nullable=False
+        ),
+        Column(
+            'amenity_id',
+            String(60),
+            ForeignKey('amenities.id'),
+            primary_key=True,
+            nullable=False
+        )
+    )
 
 
 class Place(BaseModel, Base):
-    """This is the class for Place
-    Attributes:
-        city_id: city id
-        user_id: user id
-        name: name input
-        description: string of description
-        number_rooms: number of room in int
-        number_bathrooms: number of bathrooms in int
-        max_guest: maximum guest in int
-        price_by_night:: pice for a staying in int
-        latitude: latitude in flaot
-        longitude: longitude in float
-        amenity_ids: list of Amenity ids
-    """
+    """ A place to stay """
     __tablename__ = "places"
-    city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
-    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
+
+    city_id = Column(
+        String(60),
+        ForeignKey('cities.id', ondelete="CASCADE"),
+        nullable=False)
+    user_id = Column(
+        String(60),
+        ForeignKey('users.id', ondelete="CASCADE"),
+        nullable=False)
     name = Column(String(128), nullable=False)
-    description = Column(String(1024))
-    number_rooms = Column(Integer, nullable=False, default=0)
-    number_bathrooms = Column(Integer, nullable=False, default=0)
-    max_guest = Column(Integer, nullable=False, default=0)
-    price_by_night = Column(Integer, nullable=False, default=0)
-    latitude = Column(Float)
-    longitude = Column(Float)
+    description = Column(String(1024), nullable=True)
+    number_rooms = Column(Integer, default=0, nullable=False)
+    number_bathrooms = Column(Integer, default=0, nullable=False)
+    max_guest = Column(Integer, default=0, nullable=False)
+    price_by_night = Column(Integer, default=0, nullable=False)
+    latitude = Column(Float, nullable=True)
+    longitude = Column(Float, nullable=True)
     amenity_ids = []
 
-    if getenv("HBNB_TYPE_STORAGE") == "db":
-        reviews = relationship("Review", cascade='all, delete, delete-orphan',
-                               backref="place")
 
-        amenities = relationship("Amenity", secondary=place_amenity,
-                                 viewonly=False,
-                                 back_populates="place_amenities")
+    if storage_type == 'db':
+
+        reviews = relationship(
+            "Review",
+            cascade="all, delete",
+            backref="place"
+        )
+
+        amenities = relationship(
+            "Amenity",
+            secondary="place_amenity",
+            viewonly=False,
+            back_populates="place_amenities"
+        )
     else:
+
         @property
         def reviews(self):
-            """ Returns list of reviews.id """
-            var = models.storage.all()
-            lista = []
-            result = []
-            for key in var:
-                review = key.replace('.', ' ')
-                review = shlex.split(review)
-                if (review[0] == 'Review'):
-                    lista.append(var[key])
-            for elem in lista:
-                if (elem.place_id == self.id):
-                    result.append(elem)
-            return (result)
+            """ Getter attribute to retrieve cities associated with this state """
+            from models import storage
 
+            reviews = storage.all(Place)
+            return [review for review in reviews.values() if review.place_id == self.id]
+        
         @property
         def amenities(self):
-            """ Returns list of amenity ids """
-            return self.amenity_ids
+            from models import storage  # Import DBStorage
+            from models.amenity import Amenity
 
+            amenity_objs = []
+            for amenity_id in self.amenity_ids:
+                amenity = storage.all(Amenity).get("Amenity." + amenity_id)
+                if amenity:
+                    amenity_objs.append(amenity)
+            return amenity_objs
+        
         @amenities.setter
-        def amenities(self, obj=None):
-            """ Appends amenity ids to the attribute """
-            if type(obj) is Amenity and obj.id not in self.amenity_ids:
-                self.amenity_ids.append(obj.id)
+        def amenities(self, amenity=None):
+            '''
+                Set list: amenity instances if Amenity.place_id==curr place.id
+                Set by adding instance objs to amenity_ids attribute in Place
+            '''
+            from models import storage
+            from models.amenity import Amenity
+            
+            if amenity:
+                for amenity in storage.all(Amenity).values():
+                    if amenity.place_id == self.id:
+                        self.amenity_ids.append(amenity)
---
+++ web_flask/README.md
@@ -0,0 +1,11 @@
+# Web Flask
+
+This sub-project, undertaken at AirBnB, marked my initial foray into working with Flask and Jinja2. The primary objective was to integrate the back-end storage engine with previously developed static HTML/CSS pages for the web application.
+
+## Project Overview
+
+- **Introduction to Flask:**
+  - Files 0 to 6 encompass introductory tasks aimed at acquainting myself with the basics of Flask.
+
+- **Jinja Templates:**
+  - Starting from File 7 onwards, the project progressively involved the development of more complex Jinja templates. These templates were designed to align with the structure and content of the HBnB (HomeBnB) HTML pages.
